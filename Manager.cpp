423dda812928e1b2099438bed8f0c18c79376115
Fidukj40 <jfiduk@comcast.net>
2014-10-24 12:15:48 -0400

    Create Manager.cpp

---
+++ Manager.cpp
@@ -0,0 +1,260 @@
+#pragma once
+#include <iostream>
+namespace Manage {
+	
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace std;
+
+	/// <summary>
+	/// Summary for Manager
+	/// </summary>
+	public ref class Manager : public System::Windows::Forms::Form
+	{
+		private: System::Windows::Forms::DataGridView^  dashGrid;
+				 private: static array<bool>^ options;
+
+	public:
+		Manager(void)
+		{
+			InitializeComponent();
+		}
+
+	public:
+		 Manager( DataGridView^ other, array<bool>^ options  ){
+			 dashGrid = other;
+			 this->options = options;
+			 InitializeComponent();
+		 }
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Manager()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::CheckBox^  checkBox1;
+	protected: 
+	private: System::Windows::Forms::CheckBox^  checkBox2;
+	private: System::Windows::Forms::CheckBox^  checkBox3;
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
+	private: System::Windows::Forms::ComboBox^  comboBox3;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::ComboBox^  comboBox4;
+	private: System::Windows::Forms::ComboBox^  comboBox5;
+	private: System::Windows::Forms::ComboBox^  comboBox6;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->checkBox1 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox2 = (gcnew System::Windows::Forms::CheckBox());
+			this->checkBox3 = (gcnew System::Windows::Forms::CheckBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox3 = (gcnew System::Windows::Forms::ComboBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox4 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox5 = (gcnew System::Windows::Forms::ComboBox());
+			this->comboBox6 = (gcnew System::Windows::Forms::ComboBox());
+			this->SuspendLayout();
+			// 
+			// checkBox1
+			// 
+			this->checkBox1->AutoSize = true;
+			if(options[0]){
+			this->checkBox1->CheckState = System::Windows::Forms::CheckState::Checked;
+			}
+			this->checkBox1->Location = System::Drawing::Point(12, 1);
+			this->checkBox1->Name = L"checkBox1";
+			this->checkBox1->Size = System::Drawing::Size(70, 17);
+			this->checkBox1->TabIndex = 0;
+			this->checkBox1->Text = L"Cat Scan";
+			this->checkBox1->UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this->checkBox2->AutoSize = true;
+			if(options[1]){
+			this->checkBox2->CheckState = System::Windows::Forms::CheckState::Checked;
+			}
+			this->checkBox2->Location = System::Drawing::Point(12, 84);
+			this->checkBox2->Name = L"checkBox2";
+			this->checkBox2->Size = System::Drawing::Size(41, 17);
+			this->checkBox2->TabIndex = 1;
+			this->checkBox2->Text = L"ER";
+			this->checkBox2->UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this->checkBox3->AutoSize = true;
+			if(options[2]){
+			this->checkBox3->CheckState = System::Windows::Forms::CheckState::Checked;
+			}
+			this->checkBox3->Location = System::Drawing::Point(12, 160);
+			this->checkBox3->Name = L"checkBox3";
+			this->checkBox3->Size = System::Drawing::Size(56, 17);
+			this->checkBox3->TabIndex = 2;
+			this->checkBox3->Text = L"Test 3";
+			this->checkBox3->UseVisualStyleBackColor = true;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(30) {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", 
+				L"9", L"10", L"11", L"12", L"13", L"14", L"15", L"16", L"17", L"18", L"19", L"20", L"21", L"22", L"23", L"24", L"25", L"26", 
+				L"27", L"28", L"29", L"30"});
+			this->comboBox1->Location = System::Drawing::Point(171, 36);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 3;
+			this->comboBox1->Text = L"Days";
+			this->comboBox1->SelectedIndexChanged += gcnew System::EventHandler(this, &Manager::comboBox1_SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Items->AddRange(gcnew cli::array< System::Object^  >(30) {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", 
+				L"9", L"10", L"11", L"12", L"13", L"14", L"15", L"16", L"17", L"18", L"19", L"20", L"21", L"22", L"23", L"24", L"25", L"26", 
+				L"27", L"28", L"29", L"30"});
+			this->comboBox2->Location = System::Drawing::Point(171, 112);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(121, 21);
+			this->comboBox2->TabIndex = 4;
+			this->comboBox2->Text = L"Days";
+			// 
+			// comboBox3
+			// 
+			this->comboBox3->FormattingEnabled = true;
+			this->comboBox3->Items->AddRange(gcnew cli::array< System::Object^  >(30) {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", 
+				L"9", L"10", L"11", L"12", L"13", L"14", L"15", L"16", L"17", L"18", L"19", L"20", L"21", L"22", L"23", L"24", L"25", L"26", 
+				L"27", L"28", L"29", L"30"});
+			this->comboBox3->Location = System::Drawing::Point(171, 188);
+			this->comboBox3->Name = L"comboBox3";
+			this->comboBox3->Size = System::Drawing::Size(121, 21);
+			this->comboBox3->TabIndex = 5;
+			this->comboBox3->Text = L"Days";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(171, 227);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"OK";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Manager::button1_Click);
+			// 
+			// comboBox4
+			// 
+			this->comboBox4->FormattingEnabled = true;
+			this->comboBox4->Items->AddRange(gcnew cli::array< System::Object^  >(12) {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", 
+				L"9", L"10", L"11", L"12"});
+			this->comboBox4->Location = System::Drawing::Point(21, 188);
+			this->comboBox4->Name = L"comboBox4";
+			this->comboBox4->Size = System::Drawing::Size(121, 21);
+			this->comboBox4->TabIndex = 7;
+			this->comboBox4->Text = L"Months";
+			// 
+			// comboBox5
+			// 
+			this->comboBox5->FormattingEnabled = true;
+			this->comboBox5->Items->AddRange(gcnew cli::array< System::Object^  >(12) {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", 
+				L"9", L"10", L"11", L"12"});
+			this->comboBox5->Location = System::Drawing::Point(12, 36);
+			this->comboBox5->Name = L"comboBox5";
+			this->comboBox5->Size = System::Drawing::Size(121, 21);
+			this->comboBox5->TabIndex = 8;
+			this->comboBox5->Text = L"Months";
+			// 
+			// comboBox6
+			// 
+			this->comboBox6->FormattingEnabled = true;
+			this->comboBox6->Items->AddRange(gcnew cli::array< System::Object^  >(12) {L"1", L"2", L"3", L"4", L"5", L"6", L"7", L"8", 
+				L"9", L"10", L"11", L"12"});
+			this->comboBox6->Location = System::Drawing::Point(21, 112);
+			this->comboBox6->Name = L"comboBox6";
+			this->comboBox6->Size = System::Drawing::Size(121, 21);
+			this->comboBox6->TabIndex = 9;
+			this->comboBox6->Text = L"Months";
+			// 
+			// Manager
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(398, 262);
+			this->Controls->Add(this->comboBox6);
+			this->Controls->Add(this->comboBox5);
+			this->Controls->Add(this->comboBox4);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->comboBox3);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->checkBox3);
+			this->Controls->Add(this->checkBox2);
+			this->Controls->Add(this->checkBox1);
+			this->Name = L"Manager";
+			this->Text = L"Manager";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void comboBox1_SelectedIndexChanged(System::Object^  sender, System::EventArgs^  e) {
+			 }
+
+private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+			
+		    if(checkBox1->Checked)
+			{
+			dashGrid->Columns[4]->Visible = true;
+			this->options[0] = true;
+			}
+			else{
+				dashGrid->Columns[4]->Visible = false;
+				this->options[0] = false;
+			}
+			if(checkBox2->Checked)
+			{
+			dashGrid->Columns[5]->Visible = true;
+			this->options[1] = true;
+			}
+			else{
+			dashGrid->Columns[5]->Visible = false;
+			this->options[1] = false;
+			}
+			if(checkBox3->Checked)
+			{
+			dashGrid->Columns[6]->Visible = true;
+			this->options[2] = true;
+			}
+			else{
+			dashGrid->Columns[6]->Visible = false;
+			this->options[2] = false;
+			}
+
+			Manager::Close();
+		 }
+
+};
+}
